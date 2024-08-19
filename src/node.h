ac7fc4d0c2501c9b9326efcb71c5e1228d9358eb
VincentDerk <vincent.derkinderen@gmail.com>
2024-08-19 10:54:48 +0200

    undo commit error.

--- src/node.h
+++ src/node.h
@@ -45,7 +45,7 @@ class Node {
 
 public:
     NodeType type;
-    unsigned ix;  // Index of the node in its layer; can be -1 when uninitialized.
+    int ix;  // Index of the node in its layer; can be -1 when uninitialized.
 
     std::list<Node*> children;
     std::size_t layer; // Layer index
