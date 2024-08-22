ee810589525b3dc2b678d920549d7363b987d55f
VincentDerk <vincent.derkinderen@gmail.com>
2024-08-22 17:14:57 +0200

    Moved backends to be optional (jax)

--- src/klay/__init__.py
+++ src/klay/__init__.py
@@ -1,15 +1,15 @@
 # noinspection PyUnresolvedReferences
 from .nanobind_ext import Circuit
 
-from .backends import torch_backend, jax_backend
-
 
 def to_torch_module(circuit: Circuit):
+    from .backends import torch_backend
     indices = circuit.get_indices()
     return torch_backend.KnowledgeLayer(*indices)
 
 
 def to_jax_function(circuit: Circuit):
+    from .backends import jax_backend
     indices = circuit.get_indices()
     return jax_backend.create_knowledge_layer(*indices)
 
