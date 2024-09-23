f9c688f0153bf19403d9873cd41959d3581c7634
jaron <jaron.maene@gmail.com>
2024-09-23 11:32:20 +0200

    batched nesy experiments

--- experiments/nesy/run.py
+++ experiments/nesy/run.py
@@ -1,4 +1,4 @@
-import random
+import argparse
 
 import klay
 import numpy as np
@@ -11,40 +11,39 @@ CIRCUITS = ["sudoku_4", "4-grid", "seq_fun", "warcraft_12"]
 
 def print_results(results):
     for k, v in results.items():
-        v = np.array(v) * 1000
+        v = np.array(v) * 1000  # convert to ms
         print(f"{k}: {np.mean(v):.2f} \pm {np.std(v):.2f}")
 
 
-def main():
+def main(batch_size):
     for name in CIRCUITS:
-        print("### Running", name)
+        print(f"### Running {name} (batch size {batch_size})")
         sdd_file = f"experiments/nesy/circuits/{name}.sdd"
         vtree_file = f"experiments/nesy/circuits/{name}.vtree"
 
         vtree = Vtree.from_file(vtree_file.encode())
         manager = SddManager.from_vtree(vtree)
         sdd = manager.read_sdd_file(sdd_file.encode())
-        print("Loaded SDD", sdd.count() + sdd.size())
+        print(f"Loaded SDD {sdd.count() + sdd.size()}")
 
         circuit = klay.Circuit()
         circuit.add_SDD_from_file(sdd_file)
-        print("Layerized in nodes", circuit.nb_nodes())
-        weights = [random.random() for _ in range(1000)]  # hacky, but should be enough
+        print(f"Layerized in {circuit.nb_nodes()} nodes and {len(circuit.to_torch_module().layers)} layers")
 
         for device in ['cpu', 'cuda']:
             print(f"Benchmarking Torch {device}")
-            result = benchmark_klay_torch(circuit, weights, device=device)
-            print_results(result)
-
-            print(f"Benchmarking Jax {device}")
-            result = benchmark_klay_jax(circuit, weights, device=device)
+            result = benchmark_klay_torch(circuit, 1000, 'log', device=device, batch_size=batch_size)
             print_results(result)
 
             print(f"Benchmarking Torch Naive {device}")
-            result = benchmark_sdd_torch_naive(manager, sdd, weights, device=device)
+            result = benchmark_sdd_torch_naive(manager, sdd, 1000, device=device, batch_size=batch_size)
             print_results(result)
         print()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--batch_size', type=int, default=None)
+    args = parser.parse_args()
+
+    main(args.batch_size)
--- src/klay/utils.py
+++ src/klay/utils.py
@@ -36,13 +36,11 @@ def eval_pysdd(sdd: "SddNode", weights: list[float]):
     return wmc_manager.propagate()
 
 
-def benchmark_pysdd(sdd, weights, semiring, nb_repeats=10, device='cpu'):
+def benchmark_pysdd(sdd, nb_vars, semiring, nb_repeats=10, device='cpu'):
     assert device == 'cpu'
+    pos_weights, neg_weights = python_weights(nb_vars, semiring)
     # WARNING: pysdd computes both the forward and backward passes in propagate
-    lit_weights = [1.0 - x for x in weights[::-1]] + weights
-    if semiring == "log":
-        lit_weights = [math.log(x) for x in lit_weights]
-    pysdd_weights = array('d', lit_weights)
+    pysdd_weights = array('d', neg_weights[::-1] + pos_weights)
     wmc_manager = sdd.wmc(log_mode=(semiring == "log"))
     wmc_manager.set_literal_weights_from_array(pysdd_weights)
 
@@ -149,14 +147,16 @@ def benchmark_klay_jax(circuit, nb_vars, semiring, nb_repeats=10, device='cpu'):
     return {'forward': t_forward[2:], 'backward': t_backward[2:]}
 
 
-def benchmark_klay_torch(circuit, nb_vars, semiring, nb_repeats=10, device='cpu'):
+def benchmark_klay_torch(circuit, nb_vars, semiring, nb_repeats=10, device='cpu', batch_size=None):
     circuit_forward = circuit.to_torch_module(semiring).to(device)
+    if batch_size is not None:
+        circuit_forward = torch.vmap(circuit_forward)
     circuit_forward = torch.compile(circuit_forward, mode="reduce-overhead")
 
     t_forward = []
     with torch.no_grad():
         for _ in range(nb_repeats+2):
-            weights, neg_weights = torch_weights(nb_vars, semiring,  device)
+            weights, neg_weights = torch_weights(nb_vars, semiring, device, batch_size=batch_size)
             t1 = perf_counter()
             circuit_forward(weights, neg_weights)
             if device == 'cuda':
@@ -165,20 +165,20 @@ def benchmark_klay_torch(circuit, nb_vars, semiring, nb_repeats=10, device='cpu'
 
     t_backward = []
     for _ in range(nb_repeats + 2):
-        weights, neg_weights = torch_weights(nb_vars, semiring,  device)
+        weights, neg_weights = torch_weights(nb_vars, semiring, device, batch_size=batch_size)
         t1 = perf_counter()
-        circuit_forward(weights, neg_weights).backward()
+        circuit_forward(weights, neg_weights).mean().backward()
         if device == 'cuda':
             torch.cuda.synchronize()
         t_backward.append(perf_counter() - t1)
     return {'forward': t_forward[2:], 'backward': t_backward[2:]}
 
 
-def benchmark_sdd_torch_naive(manager, sdd, nb_vars, nb_repeats=10, device='cpu'):
+def benchmark_sdd_torch_naive(manager, sdd, nb_vars, nb_repeats=10, device='cpu', batch_size=None):
     t_forward = []
     with torch.inference_mode():
         for _ in range(nb_repeats+2):
-            weights, neg_weights = torch_weights(nb_vars, 'log',  device)
+            weights, neg_weights = torch_weights(nb_vars, 'log',  device, batch_size=batch_size)
             t1 = perf_counter()
             eval_sdd_torch_naive(manager, sdd, weights, neg_weights, device)
             if device == 'cuda':
@@ -187,9 +187,9 @@ def benchmark_sdd_torch_naive(manager, sdd, nb_vars, nb_repeats=10, device='cpu'
 
     t_backward = []
     for _ in range(nb_repeats + 2):
-        weights, neg_weights = torch_weights(manager.var_count(), 'log',  device)
+        weights, neg_weights = torch_weights(manager.var_count(), 'log',  device, batch_size=batch_size)
         t1 = perf_counter()
-        eval_sdd_torch_naive(manager, sdd, weights, neg_weights, device).backward()
+        eval_sdd_torch_naive(manager, sdd, weights, neg_weights, device).mean().backward()
         if device == 'cuda':
             torch.cuda.synchronize()
         t_backward.append(perf_counter() - t1)
@@ -204,9 +204,9 @@ def eval_sdd_torch_naive(manager, sdd, pos_weights, neg_weights, device):
             if node.is_literal():
                 literal = node.literal
                 if literal < 0:
-                    return neg_weights[-literal - 1]
+                    return neg_weights[..., -literal - 1]
                 else:
-                    return pos_weights[literal - 1]
+                    return pos_weights[..., literal - 1]
             elif node.is_true():
                 return torch.tensor(0., device=device)
             elif node.is_false():
@@ -218,10 +218,17 @@ def eval_sdd_torch_naive(manager, sdd, pos_weights, neg_weights, device):
     return result
 
 
-def torch_weights(nb_vars, semiring = 'log', device='cpu'):
+def torch_weights(nb_vars, semiring = 'log', device='cpu', batch_size=None):
     weights, neg_weights = python_weights(nb_vars, semiring)
     weights = torch.tensor(weights, dtype=torch.float32, device=device)
     neg_weights = torch.tensor(neg_weights, dtype=torch.float32, device=device)
+    if batch_size is not None:
+        weights = weights.repeat(batch_size, 1)
+        weights.uniform_(0, 1)
+        neg_weights = 1 - weights
+        if semiring == 'log':
+            weights = weights.log()
+            neg_weights = neg_weights.log()
     weights.requires_grad = True
     neg_weights.requires_grad = True
     return weights, neg_weights
