4b068e64f3b52bcbb5ac4198f7b8ee9a2d8b8981
Jaron Maene <jaron.maene@gmail.com>
2025-03-14 16:17:10 -0700

    make get_indices private

--- src/klay/__init__.py
+++ src/klay/__init__.py
@@ -4,13 +4,13 @@ from .nanobind_ext import Circuit
 
 def to_torch_module(circuit: Circuit, semiring: str = "log"):
     from .backends import torch_backend
-    indices = circuit.get_indices()
+    indices = circuit._get_indices()
     return torch_backend.KnowledgeModule(*indices, semiring=semiring)
 
 
 def to_jax_function(circuit: Circuit, semiring: str = "log"):
     from .backends import jax_backend
-    indices = circuit.get_indices()
+    indices = circuit._get_indices()
     return jax_backend.create_knowledge_layer(*indices, semiring=semiring)
 
 
