ec25b3ebfb2ed05c115f6de4c1fb12c59eb7fda7
Jaron Maene <jaron.maene@gmail.com>
2025-03-14 16:53:37 -0700

    clean up docstrings a bit

--- src/klay/__init__.py
+++ src/klay/__init__.py
@@ -1,25 +1,42 @@
 # noinspection PyUnresolvedReferences
 from .nanobind_ext import Circuit
 
+from collections.abc import Sequence
 
-def to_torch_module(circuit: Circuit, semiring: str = "log"):
+
+def to_torch_module(self: Circuit, semiring: str = "log"):
+    """
+    Convert the circuit into a PyTorch module.
+
+    :param semiring:
+        The semiring in which the circuit should be evaluated. Supported options are ("log", "real", "mpe", "godel").
+    """
     from .backends import torch_backend
-    indices = circuit._get_indices()
+    indices = self._get_indices()
     return torch_backend.KnowledgeModule(*indices, semiring=semiring)
 
 
-def to_jax_function(circuit: Circuit, semiring: str = "log"):
+def to_jax_function(self: Circuit, semiring: str = "log"):
+    """
+    Convert the circuit into a Jax function.
+
+    :param semiring:
+        The semiring in which the circuit should be evaluated. Supported options are ("log", "real").
+    """
     from .backends import jax_backend
-    indices = circuit._get_indices()
+    indices = self._get_indices()
     return jax_backend.create_knowledge_layer(*indices, semiring=semiring)
 
 
-def add_sdd(circuit: Circuit, sdd: "SddNode", **kwargs):
+def add_sdd(self: Circuit, sdd: "SddNode", true_lits: Sequence[int] = (), false_lits: Sequence[int] = ()):
+    """
+    Add an SDD to the Circuit.
+    """
     import os
     from pathlib import Path
 
     sdd.save(bytes(Path("tmp.sdd")))
-    circuit.add_SDD_from_file("tmp.sdd", **kwargs)
+    self.add_SDD_from_file("tmp.sdd", true_lits, false_lits)
     os.remove("tmp.sdd")
 
 
